9ff875121d5d7783ba39f3017a7a965eaf80870a
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-23 00:59:25 +0300

    User to be added

--- models/user.py
+++ models/user.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from sqlalchemy.orm import declarative_base
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+#from sqlalchemy.orm import relations
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    if models.storage_type == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        # places = relationship("Place", back_populates="user",
+        #                       cascade="all, delete-orphan")
+        # reviews = relationship("Review", backref="user",
+        #                       cascade="all, delete-orphan")
+
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
