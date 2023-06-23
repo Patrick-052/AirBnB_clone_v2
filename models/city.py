5484717107b375e5a37ed0c76f08ec716ed22e30
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-23 15:28:39 +0300

    inspecting the console

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 import sqlalchemy
 from models.state import State
@@ -8,6 +9,13 @@ from sqlalchemy import Column, String, ForeignKey
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey(State.id), nullable=False)
-    name = Column(String(128), nullable=False)
+    if models.storage_type == "db":
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        # state = relationship("State", back_populates="cities")
+
+    else:
+        state_id = ""
+        name=""
+
--- models/state.py
+++ models/state.py
@@ -10,16 +10,19 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state",
-                          cascade="all, delete, delete-orphan")
+    if models.storage_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """
-        Return the list of ``City`` instances with ``state_id`` equal to
-        the current ``State.id``
-        """
-        return [value for key, value in models.storage.all().items()
-                if key.split(".")[0] == "City" and value.state_id == self.id]
+        @property
+        def cities(self):
+            """
+            Return the list of ``City`` instances with ``state_id`` equal to
+            the current ``State.id``
+            """
+            return [value for key, value in models.storage.all().items()
+                    if key.split(".")[0] == "City" and value.state_id == self.id]
