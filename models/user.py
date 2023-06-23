097b27e6dc0903048eb6b3c437f19cb09a1723f9
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-23 23:20:18 +0300

    creating a new user table

--- models/user.py
+++ models/user.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy.orm import declarative_base
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
-from sqlalchemy.orm import relationship
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import backref, relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_type == "db":
-        __tablename__ = "users"
+    if models.storage_type == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", back_populates="user",
-                              cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete-orphan")
-
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user')
+        reviews = relationship("Review", backref="user")
     else:
         email = ''
         password = ''
