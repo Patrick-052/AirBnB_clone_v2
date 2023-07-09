8ba026386cd3a8f01077527bac87c3f42511b9ac
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-09 21:38:52 +0000

    Creating an archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,9 +14,12 @@ def do_pack():
     arch_path = 'versions'
     frmt = datetime.now().strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(frmt)
-    path = os.path.join(arch_path, archive_name)
-    result = local("tar -czvf {} web_static".format(path))
+    tgz_path = os.path.join(arch_path, archive_name)
+    result = local("tar -czvf {} web_static".format(tgz_path))
+    file_size = os.stat(tgz_path).st_size
+    print("web_static packed: versions/web_static_{}.tgz -> {}Bytes"
+          .format(frmt, file_size))
 
     if result.failed:
         return None
-    return path
+    return tgz_path
