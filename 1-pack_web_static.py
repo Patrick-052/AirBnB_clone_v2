1432af5a67f1013bf66cd1d8932720a50f154033
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-09 20:22:01 +0300

    Creating an archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,10 +11,14 @@ def do_pack():
     """function containing commands and process to generate the
        .tgz archive"""
     local("mkdir -p versions")
-    frmt = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = f"web_static_{frmt}.tgz"
-    path = os.path.join("version", archive_name)
-    result = local(f"tar -czvf {path} web_static")
+    # frmt = datetime.now().strftime("%Y%m%d%H%M%S")
+    # archive_name = f"web_static_{frmt}.tgz"
+    # path = os.path.join("version", archive_name)
+    # result = local(f"tar -czvf {path} web_static")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
 
     if result.failed:
         return None
