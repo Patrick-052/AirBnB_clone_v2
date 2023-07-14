204d637979d305c87e0998c1eb9a3ff5458ec5a7
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-14 06:53:53 +0000

    Creating an archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,20 +5,17 @@
 import os
 from fabric.api import local
 from datetime import datetime
-from fabric.decorators import runs_once
 
 
-@runs_once
 def do_pack():
     """function containing commands and process to generate the
        .tgz archive"""
     local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
+    frmt = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(frmt)
+    tgz_path = "versions/{}".format(archive_name)
+    result = local("tar -czvf {} web_static".format(tgz_path))
 
     if result.failed:
         return None
-    return path
-
+    return tgz_path
