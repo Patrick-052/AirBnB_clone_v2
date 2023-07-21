2508fdad003f9f90f7fc3f44e6ad1691966ef5ab
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-21 20:08:13 +0300

    Path with a default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Starting a Flask web application and ensuring
+   that its listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns a specific string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns a specific string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """passing a variable to the url"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/", defaults={"text": "is cool"},
+           strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """return value of variable to a url else return
+    default"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=None)
