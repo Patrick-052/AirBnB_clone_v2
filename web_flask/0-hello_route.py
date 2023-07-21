2860ccb7db0a9b95ef60aad3e9cf3dd882a9645c
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-21 16:57:49 +0300

    Starting a Flask web-app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Starting a Flask web application"""
+"""Starting a Flask web application and ensuring
+   that its listening on 0.0.0.0, port 5000"""
 
 from flask import Flask
+
 app = Flask("__name__")
 
 
@@ -10,5 +12,5 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=None)
