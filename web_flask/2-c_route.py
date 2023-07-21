1cf99cf468d17c446af0e3c2eaf23bbfc9c0be85
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-21 18:32:32 +0300

    Adding to the stack of task 0 and 1

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starting a Flask web application and ensuring
+   that its listening on 0.0.0.0, port 5000 and
+   displaying different strings when visiting the urls
+   also passing variables to a url"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    "returns a specific string"
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    "returns a specific string"
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    "passing a variable to the url"
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=None)
