93129502812aa101787189b4e6a001ab73ca6c4b
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-21 19:25:12 +0300

    More paths to visit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,6 +12,7 @@ app = Flask("__name__")
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
@@ -19,4 +20,3 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=None)
-
