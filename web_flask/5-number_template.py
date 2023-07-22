6f8d2654096de1bbc460a09da2c5a1935668aba2
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-22 15:46:07 +0300

    update from task 4 stack

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
@@ -49,4 +49,4 @@ def number_template(n=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=None)
+    app.run()
