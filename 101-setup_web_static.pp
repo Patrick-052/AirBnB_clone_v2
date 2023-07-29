9dca37c47b25b6b7fab012e00e782e5d3cc26e33
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-29 22:57:27 +0300

    change of configurations

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,29 @@
-# configuring nginx
+# AirBnB clone web server setup and configuration
+
+# SCRIPT INCOMPLETE. NEEDS SOME MORE THINKING---
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
 package { 'nginx':
-  ensure => present,
+  ensure   => 'present',
+  provider => 'apt'
 }
 
 -> file { '/data':
@@ -23,15 +46,9 @@ package { 'nginx':
   ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test/index.html':
-  content =>
-  "<html>
-    <head>
-    </head>
-    <body>
-      Well my config file works fine!!
-    </body>
-  </html>",
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
 
 -> file { '/data/web_static/current':
@@ -43,12 +60,27 @@ file { '/data/web_static/releases/test/index.html':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-file_line { 'append_after_pattern':
-  ensure =>  present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => 'location /hbnb_static {\n\talias /data/web_static/current/;\n\t}',
-  match  => 'server_name _;',
-  notify => Service['nginx'],
+file { '/var/www':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
 }
 
 -> exec { 'nginx restart':
