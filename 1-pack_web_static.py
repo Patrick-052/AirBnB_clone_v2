4c17a0134afdef783eed2650a579a64f77c143d1
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-29 11:41:58 +0000

    Creating an archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""""Generating a .tgz archive from the contents of
-    the web_static folder of my AirBnB Cloned repo"""
+'''Fabric script to generate .tgz archive'''
 
-import os
 from fabric.api import local
 from datetime import datetime
 
+from fabric.decorators import runs_once
 
+
+@runs_once
 def do_pack():
-    """function containing commands and process to generate the
-       .tgz archive"""
+    '''generates .tgz archive from the contents of the web_static folder'''
     local("mkdir -p versions")
-    frmt = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "web_static_{}.tgz".format(frmt)
-    tgz_path = "versions/{}".format(archive_name)
-    result = local("tar -czvf {} web_static".format(tgz_path))
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
 
     if result.failed:
         return None
-    return tgz_path
+    return path
+
