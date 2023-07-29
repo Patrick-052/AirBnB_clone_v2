e5cee0ea8c53c40abd29100828c78c94a2fd6aaa
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-29 11:57:41 +0000

    Ensuring all files are compresssed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
+""""Generating a .tgz archive from the contents of
+    the web_static folder of my AirBnB Cloned repo"""
 
 from fabric.api import local
 from datetime import datetime
-
 from fabric.decorators import runs_once
 
 
 @runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
+    """function containing commands and process to generate the
+       .tgz archive"""
     local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
+    frmt = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(frmt)
+    tgz_path = "versions/{}".format(archive_name)
+    result = local("tar -czvf {} web_static".format(tgz_path))
 
     if result.failed:
         return None
-    return path
+    return tgz_path
 
