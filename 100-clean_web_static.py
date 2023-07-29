e65c4b15f7df64924d440fd1da849212be9eaa5c
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-30 00:39:24 +0300

    cleaning up

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """deleting out-of-date archives"""
 
-from fabric.api import env
+from fabric.api import env, run, local, lcd
 
 
 env.user = 'ubuntu'
@@ -11,10 +11,11 @@ env.hosts = ['34.232.67.39', '18.233.63.201']
 
 def do_clean(number=0):
     """Cleaning up"""
-    if number == 0 || number == 1 || number == 2:
-       pass
-    else:
-       with lcd("versions/"):
-           local("ls -lt | awk 'NR >= 3' | xargs rm -f")
-       run("rm -f /data/web_static/releases/{}*".format(endswith(".tgz")))
-       print("Am done cleaning up!!")
+    if number < 1:
+        print("Number must be greater than or equal to 1")
+        return
+    with lcd("versions/"):
+        local("ls -lt | tail -n +{} | xargs rm -f".format(number + 1))
+    run("ls -t /data/web_static/releases/ | tail -n +{} | xargs -I {{}}\
+        rm -rf /data/web_static/releases/{{}}".format(number + 1))
+    print("Done cleaning up!!")
