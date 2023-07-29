c1112e2c91237b91011cf0e9cad892302a0d0b35
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-29 13:37:05 +0000

    code deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Deploying code to the servers"""
-from fabric.api import sudo, put, env
+from fabric.api import put, env, run
 import os
 
 
@@ -16,13 +16,19 @@ def do_deploy(archive_path):
        return False
 
     filename = os.path.basename(archive_path)
-    folder_name = "/data/web_static/releases/" + filename[:-4]
-    put(archive_path, "/tmp/")
-    sudo("mkdir -p {}".format(folder_name))
-    sudo("tar -xzvf /tmp/{} -C {}".format(filename, folder_name))
-    sudo("rm -f /tmp/{}".format(filename))
-    sudo("rm -rf /data/web_static/current")
-    sudo("ln -s {} /data/web_static/current".format(folder_name))
-    
-    return True
-
+    folder_name = "/data/web_static/releases/{}/".format(filename[:-4])
+   
+    try:
+        put(archive_path, "/tmp/{}".format(filename))
+        run("mkdir -p {}".format(folder_name))
+        run("tar -xzvf /tmp/{} -C {}".format(filename, folder_name))
+        run("rm -f /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(folder_name, folder_name))
+        run("rm -rf {}web_static".format(folder_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_name))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
